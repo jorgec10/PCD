40337745d0e00e18d629afd916e91c0bfb06a209
Jorge Gallego Madrid <jorge.gallego.madrid@gmail.com>
2016-05-09 23:43:14 +0200

    run del mult2

--- PCD/ejercicio2/src/HiloMultiplosDos.java
+++ PCD/ejercicio2/src/HiloMultiplosDos.java
@@ -7,6 +7,12 @@ public class HiloMultiplosDos extends Thread {
 
     @Override
     public void run() {
-
+        int cont = 0;
+        for (int i = 0; i < Main.MAX_NUM; i++) {
+            if (Main.sharedBuffer[i%Main.SHARED_BUFFER_SIZE]%2 == 0) {
+                inBuffer[cont] = Main.sharedBuffer[i % Main.SHARED_BUFFER_SIZE];
+                cont++;
+            }
+        }
     }
 }
